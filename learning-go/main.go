4f62218b1f25451081b6828726c30ad33ad69b88
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-19 11:58:32 +0300

    Golang Web

--- learning-go/main.go
+++ learning-go/main.go
@@ -5,12 +5,16 @@ import "log"
 func main() {
 	var whatToSay string
 	var saySomeThingElse string
+	var i int
+
 	whatToSay = saySomething("Hello, World!")
 	log.Println(whatToSay)
 
 	saySomething("GoodBye")
+
 	log.Println(saySomeThingElse)
 	log.Println(saySomething("Finally"))
+	log.Println(i)
 
 }
 
